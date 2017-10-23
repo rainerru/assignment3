46b514b0d18e85d2f026f771dcfd16736030c40a
rainerru <rainerru@stud.sbg.ac.at>
2017-10-23 23:53:08 +0200

    added commentary for CustomStack

--- CustomStack.java
+++ CustomStack.java
@@ -6,23 +6,47 @@ public class CustomStack<T> {
 	protected T onTop;
 	protected CustomStack<T> below;
 
+	/**
+	 * creates an empty stack
+	 */
 	public CustomStack () {}
 
+	/**
+	 * creates a stack containing a single element
+	 *
+	 * @param	firstElement	the element which will be the only element on the stack
+	 */
 	public CustomStack ( T firstElement )
 	{
 		this.onTop = firstElement;
 	}
 
+	/**
+	 * creates a stack with at least two elements
+	 *
+	 * @param	firstElement	the element which will be on top of the stack
+	 * @param	theStackBelow	the stack which will be below the top of the stack
+	 */
 	public CustomStack ( T firstElement, CustomStack<T> theStackBelow )
 	{
 		this.onTop = firstElement;
 		this.below = theStackBelow;
 	}
 
+	/**
+	 * return true if the stack contains no elements
+	 *
+	 * @return	true if the stack has no elements, false otherwise
+	 */
 	public boolean isEmpty () {
 		return onTop == null;
 	}
 
+	/**
+	 * returns the object that is on top of the stack
+	 *
+	 * @return	the object on top of the stack
+	 */
 	public T peek ()
 	{
 		if ( this.isEmpty() )
@@ -33,11 +57,21 @@ public class CustomStack<T> {
 		return this.onTop;
 	}
 
+	/**
+	 * returns the stack that is below the top of the stack
+	 *
+	 * @return	the stack below the top of the stack
+	 */
 	public CustomStack<T> peekBelow ()
 	{
 		return this.below;
 	}
 
+	/**
+	 * removes the object on top of the stack and returns it
+	 *
+	 * @return	the object on top of the stack
+	 */
 	public T pop ()
 	{
 		if ( this.isEmpty() )
@@ -56,6 +90,11 @@ public class CustomStack<T> {
 		return returnObject;
 	}
 
+	/**
+	 * adds an element to the stack
+	 *
+	 * @param	newItem	the object which will be added on top of the stack
+	 */
 	public void push ( T newItem )
 	{
 		if ( this.onTop == null )	this.onTop = newItem;
@@ -67,6 +106,14 @@ public class CustomStack<T> {
 		}
 	}
 
+	/**
+	 * searches for an item in the stack and returns the position of the item counting
+	 * from the top down, if this item is indeed in the stack. Otherwise, a RunTimeExcpetion
+	 * (more specifically, a NoSuchElementException) occurs.
+	 *
+	 * @param	item	the item which will be searched
+	 * @return			the position of the item counting from the top down
+	 */
 	public int search ( T item )
 	{
 		int counter = 0;
